30aaf8d7a1b5c2b5555ef673492349139b29ec72
lasse-cs <lasse.schmieding@gmail.com>
2025-04-23 20:39:10 +0100

    Add an abstract page model

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 
-class Article(models.Model):
+class Page(models.Model):
     slug = models.SlugField(max_length=50, primary_key=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
 
+    class Meta:
+        abstract = True
+
+
+class Article(Page):
     def get_absolute_url(self):
         return reverse("article", args=[self.slug])
