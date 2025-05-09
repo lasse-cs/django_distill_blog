2c149a9f7af361164b6a54e81900c7effdf5b02d
lasse-cs <lasse.schmieding@gmail.com>
2025-05-09 11:08:44 +0100

    Add a basic admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,31 @@
-# Register your models here.
+from django.contrib import admin
+
+from blog.models import Article, Author, NavPage, Tag
+
+
+class TagInline(admin.StackedInline):
+    model = Article.tags.through
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        TagInline,
+    ]
+    exclude = ("tags",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(NavPage)
+class NavPageAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
