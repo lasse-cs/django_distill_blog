b6662369db1d28cb6e0b15c4709efcee154813f0
lasse-cs <lasse.schmieding@gmail.com>
2025-05-10 08:41:55 +0100

    Change tests to use POMs

---
+++ functional_tests/pages.py
@@ -0,0 +1,189 @@
+import re
+from playwright.sync_api import expect
+
+
+class Navbar:
+    def __init__(self, page, locator):
+        self.page = page
+        self.locator = locator
+
+    def go_to_index(self):
+        index_link = self.locator.get_by_role("link", name="Blog")
+        expect(index_link).to_be_visible()
+        index_link.click()
+        return ArticleListPage(self.page)
+
+    def go_to_nav_page(self, nav_page):
+        nav_page_link = self.locator.get_by_role("link", name=nav_page.title)
+        expect(nav_page_link).to_be_visible()
+        nav_page_link.click()
+        return NavPagePage(self.page)
+
+    def expect_to_be_visible(self):
+        expect(self.locator).to_be_visible()
+
+
+class ArticleListPage:
+    def __init__(self, page):
+        self.page = page
+        self.heading = page.get_by_role("heading", name="All Articles")
+
+        navbar_locator = page.get_by_role("navigation", name="Main Navigation")
+        self.navbar = Navbar(page, navbar_locator)
+
+        article_list_locator = page.get_by_role("main")
+        self.article_list = ArticleList(page, article_list_locator)
+
+    def expect_to_have_articles(self, articles):
+        self.article_list.expect_to_have_length(len(articles))
+        for article in articles:
+            self.article_list.expect_to_have_article(article)
+
+    def expect_to_be_loaded(self):
+        self.navbar.expect_to_be_visible()
+        expect(self.page).to_have_title(re.compile("Blog"))
+        expect(self.heading).to_be_visible()
+
+    def visit_article(self, article):
+        return self.article_list.visit_article(article)
+
+    def visit_tag_on_article(self, article, tag):
+        return self.article_list.visit_tag_on_article(article, tag)
+
+    def get_content_for_article(self, article):
+        return self.article_list.get_content_for_article(article)
+
+
+class TagList:
+    def __init__(self, locator):
+        self.tag_locators = locator.get_by_role("listitem")
+
+    def expect_to_have_length(self, length):
+        expect(self.tag_locators).to_have_count(length)
+
+    def expect_to_have_tag(self, tag):
+        tag_link = self.tag_locators.get_by_role("link", name=tag.name)
+        expect(tag_link).to_be_visible()
+
+    def expect_to_have_tags(self, tags):
+        self.expect_to_have_length(len(tags))
+        for tag in tags:
+            tag_link = self.tag_locators.get_by_role("link", name=tag.name)
+            expect(tag_link).to_be_visible()
+
+    def visit_tag(self, tag):
+        tag_link = self.tag_locators.get_by_role("link", name=tag.name)
+        tag_link.click()
+        return TagPage(tag_link.page)
+
+
+class TagPage:
+    def __init__(self, page):
+        self.page = page
+        navbar_locator = page.get_by_role("navigation", name="Main Navigation")
+        self.navbar = Navbar(page, navbar_locator)
+
+        article_list_locator = page.get_by_role("main")
+        self.article_list = ArticleList(page, article_list_locator)
+
+    def expect_to_be_for_tag(self, tag):
+        heading = self.page.get_by_role("heading", level=1)
+        expect(heading).to_contain_text(tag.name)
+
+        articles = tag.article_set.all()
+        self.article_list.expect_to_have_length(len(articles))
+        for article in articles:
+            self.article_list.expect_to_have_article(article)
+
+
+class ArticleList:
+    def __init__(self, page, locator):
+        self.page = page
+        self.article_locators = locator.get_by_role("article")
+
+    def expect_to_have_length(self, length):
+        expect(self.article_locators).to_have_count(length)
+
+    def _get_article_element(self, article):
+        title_locator = self.page.get_by_role("heading", name=article.title)
+        return self.article_locators.filter(has=title_locator)
+
+    def expect_to_have_article(self, article):
+        article_element = self._get_article_element(article)
+        expect(article_element).to_be_visible()
+
+        header_group = article_element.locator("hgroup")
+        expect(header_group).to_contain_text(article.author.name)
+
+        article_link = article_element.get_by_role("link", name=article.title)
+        expect(article_link).to_be_visible()
+        tags_list = TagList(article_element.locator("footer"))
+        tags_list.expect_to_have_tags(article.tags.all())
+
+    def visit_article(self, article):
+        article_element = self._get_article_element(article)
+        article_link = article_element.get_by_role("link", name=article.title)
+        article_link.click()
+
+        return ArticlePage(self.page)
+
+    def visit_tag_on_article(self, article, tag):
+        article_element = self._get_article_element(article)
+        tags = TagList(article_element.locator("footer"))
+        return tags.visit_tag(tag)
+
+    def get_content_for_article(self, article):
+        article_element = self._get_article_element(article)
+        content = article_element.locator("section")
+        return content
+
+
+class ArticlePage:
+    def __init__(self, page):
+        self.page = page
+        self.article_locator = page.get_by_role("article")
+        self.header_group = self.article_locator.locator("hgroup")
+        self.title = self.header_group.get_by_role("heading")
+
+        navbar_locator = page.get_by_role("navigation", name="Main Navigation")
+        self.navbar = Navbar(page, navbar_locator)
+
+        tag_locator = self.article_locator.locator("footer")
+        self.tag_list = TagList(tag_locator)
+
+    def expect_to_be_for_article(self, article):
+        expect(self.page).to_have_title(re.compile(article.title))
+        self.navbar.expect_to_be_visible()
+
+        expect(self.title).to_be_visible()
+        expect(self.title).to_have_text(article.title)
+
+        author = self.header_group.get_by_text(article.author.name)
+        expect(author).to_be_visible()
+
+        self.tag_list.expect_to_have_tags(article.tags.all())
+
+    def get_article_content(self):
+        content = self.article_locator.locator("section")
+        return content
+
+    def visit_tag(self, tag):
+        return self.tag_list.visit_tag(tag)
+
+
+class NavPagePage:
+    def __init__(self, page):
+        self.page = page
+        nav_bar_locator = page.get_by_role("navigation", name="Main Navigation")
+        self.navbar = Navbar(page, nav_bar_locator)
+
+    def expect_to_be_for_nav_page(self, nav_page):
+        expect(self.page).to_have_title(re.compile(nav_page.title))
+        self.navbar.expect_to_be_visible()
+
+        title = self.page.get_by_role("heading", name=nav_page.title)
+        expect(title).to_be_visible()
+
+    def get_page_content(self):
+        content = self.page.locator("section")
+        return content
--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,66 +1,31 @@
-import re
 from playwright.sync_api import expect
 import pytest
+
+from functional_tests.pages import ArticleListPage, NavPagePage
 
 
 pytestmark = pytest.mark.django_db
 
 
-def test_index_page(page, server_url, articles):
-    # There is a great new blog in town
+def test_visit_index_and_article_page(page, server_url, articles):
+    # There is a new blog in town
     # L goes to visit it:
     page.goto(server_url)
 
-    # He notices that it has Blog in the title
-    expect(page).to_have_title(re.compile("Blog"))
+    # He lands on the list page of articles
+    list_page = ArticleListPage(page)
+    list_page.expect_to_be_loaded()
+    list_page.expect_to_have_articles(articles)
 
-    # He is also notices that there is a showing that this lists the articles
-    title = page.get_by_role("heading", name="All Articles")
-    expect(title).to_be_visible()
-
-    # There is a list of articles
-    article_list = page.get_by_role("article")
-    expect(article_list).to_have_count(len(articles))
-    for article in articles:
-        article_element = article_list.filter(has_text=article.title)
-        # Each article has a title
-        title = article_element.get_by_role("heading", name=article.title)
-        expect(title).to_be_visible()
-
-        # Each article has an author
-        author = article_element.get_by_text(article.author.name)
-        expect(author).to_be_visible()
-
-        # Each article has a link to the article
-        link = article_element.get_by_role("link", name=article.title)
-        expect(link).to_be_visible()
-
-    # Clicking on the title of an article takes him to a dedicated page for the
-    # article
+    # He visits the first article
     first_article = articles[0]
-    article_link = page.get_by_role("link", name=first_article.title)
-    article_link.click()
+    article_page = list_page.visit_article(first_article)
+    article_page.expect_to_be_for_article(first_article)
 
-    # The article page has the title of the article
-    expect(page).to_have_title(re.compile(first_article.title))
-    title = page.get_by_role("heading", name=first_article.title)
-    expect(title).to_be_visible()
-
-    # The article page has the author of the article
-    author = page.get_by_text(first_article.author.name)
-    expect(author).to_be_visible()
-
-    # There is a navbar
-    navbar = page.get_by_role("navigation", name="Main Navigation")
-    expect(navbar).to_be_visible()
-
-    # With a link to the index page
-    index_link = navbar.get_by_role("link", name="Blog")
-    index_link.click()
-
-    # The index page has the title of the blog
-    expect(page).to_have_title(re.compile("Blog"))
-    title = page.get_by_role("heading", name="Articles")
+    # From the article page, he can go back to the index page
+    list_page = article_page.navbar.go_to_index()
+    list_page.expect_to_be_loaded()
+    list_page.expect_to_have_articles(articles)
 
 
 def test_main_navigation(page, server_url, about_page):
@@ -68,130 +33,64 @@ def test_main_navigation(page, server_url, about_page):
     page.goto(server_url)
 
     # He is on the main index page
-    expect(page).to_have_title(re.compile("Blog"))
-    title = page.get_by_role("heading", name="All Articles")
-    expect(title).to_be_visible()
+    list_page = ArticleListPage(page)
+    list_page.expect_to_be_loaded()
 
-    # He sees there is a navbar at the top of the page
-    navbar = page.get_by_role("navigation", name="Main Navigation")
-    expect(navbar).to_be_visible()
+    # He can visit the about page through the navbar
+    about_page_page = list_page.navbar.go_to_nav_page(about_page)
+    about_page_page.expect_to_be_for_nav_page(about_page)
 
-    # The navbar has a link to the index page
-    index_link = navbar.get_by_role("link", name="Blog")
-    expect(index_link).to_be_visible()
-
-    # The navbar also has a link to an about page
-    about_link = navbar.get_by_role("link", name="About")
-    expect(about_link).to_be_visible()
-    about_link.click()
-
-    # He is taken to the about page
-    expect(page).to_have_title(re.compile("About"))
-    title = page.get_by_role("heading", name="About")
-    expect(title).to_be_visible()
-
-    # The about page still has the navbar
-    navbar = page.get_by_role("navigation", name="Main Navigation")
-    expect(navbar).to_be_visible()
-
-    # The navbar has a link to the index page
-    index_link = navbar.get_by_role("link", name="Blog")
-    expect(index_link).to_be_visible()
-    index_link.click()
-
-    # He is taken back to the index page
-    expect(page).to_have_title(re.compile("Blog"))
-    title = page.get_by_role("heading", name="All Articles")
-    expect(title).to_be_visible()
+    # He can go back to the index page through the navbar
+    list_page = about_page_page.navbar.go_to_index()
+    list_page.expect_to_be_loaded()
 
 
 def test_tags(page, server_url, tagged_articles):
     # L goes to visit the blog page
     page.goto(server_url)
 
-    # He sees that the articles have tags
-    article_list = page.get_by_role("article")
-    for article in tagged_articles:
-        # Each article has a list of tags
-        article_element = article_list.filter(has_text=article.title)
-        tags = article_element.get_by_role("listitem")
-        expect(tags).to_have_count(len(article.tags.all()))
-        for tag in article.tags.all():
-            # Each tag has a link to the tag page
-            tag_link = tags.get_by_role("link", name=tag.name)
-            expect(tag_link).to_be_visible()
+    # He lands on the list page of articles
+    list_page = ArticleListPage(page)
+    list_page.expect_to_be_loaded()
 
-    # Going to the article page, the tags are also visible there
+    # He can click on a tag in the article list
+    # and be taken to the tag page
     first_article = tagged_articles[0]
-    article_link = page.get_by_role("link", name=first_article.title)
-    article_link.click()
+    first_tag = first_article.tags.first()
+    tag_page = list_page.visit_tag_on_article(first_article, first_tag)
+    tag_page.expect_to_be_for_tag(first_tag)
 
-    # The article page has the title of the article
-    expect(page).to_have_title(re.compile(first_article.title))
+    # From the tag page, he can go back to the index page
+    list_page = tag_page.navbar.go_to_index()
+    list_page.expect_to_be_loaded()
 
-    tags = page.get_by_role("article").get_by_role("listitem")
-    for tag in first_article.tags.all():
-        tag_link = tags.get_by_role("link", name=tag.name)
-        expect(tag_link).to_be_visible()
+    # He can visit an article from the index page
+    article_page = list_page.visit_article(first_article)
+    article_page.expect_to_be_for_article(first_article)
 
-    # Clicking on a tag, takes him to a page that lists the articles with that tag.
-    first_tag = first_article.tags.all()[0]
-    tags.get_by_role("link", name=first_tag.name).click()
-
-    # The tag page has a heading with the name of the tag
-    title = page.get_by_role("heading", name=f'Articles tagged "{first_tag.name}"')
-    expect(title).to_be_visible()
-    # The page has a list of articles with that tag
-    article_list = page.get_by_role("article")
-    expect(article_list).to_have_count(len(first_tag.article_set.all()))
-    for article in first_tag.article_set.all():
-        # Each article has a title
-        title = article_list.get_by_role("heading", name=article.title)
-        expect(title).to_be_visible()
-
-    # The page has a link to the index page
-    index_link = page.get_by_role("link", name="Blog")
-    expect(index_link).to_be_visible()
-    index_link.click()
-
-    # He is taken back to the index page
-    expect(page).to_have_title(re.compile("Blog"))
-    title = page.get_by_role("heading", name="All Articles")
-    expect(title).to_be_visible()
-
-    # Going back to the index page, he can also go to the tag page from there
-    # by clicking on the tag in the article list.
-    article_list = page.get_by_role("article")
-    first_article = tagged_articles[0]
-    article_element = article_list.filter(has_text=first_article.title)
-    tags = article_element.get_by_role("listitem")
-    first_tag = first_article.tags.all()[0]
-    tags.get_by_role("link", name=first_tag.name).click()
-
-    # The tag page has a heading with the name of the tag
-    title = page.get_by_role("heading", name=f'Articles tagged "{first_tag.name}"')
-    expect(title).to_be_visible()
+    # From the article page, he can also visit the tag page
+    tag_page = article_page.visit_tag(first_tag)
+    tag_page.expect_to_be_for_tag(first_tag)
 
 
-def test_markdown_article(page, server_url, markdown_article):
-    # L goes to visit the blog page
+def test_markdown_article_content(page, server_url, markdown_article):
     page.goto(server_url)
 
-    # He sees that the articles have markdown
-    article_list = page.get_by_role("article")
-    article_element = article_list.filter(has_text=markdown_article.title)
+    list_page = ArticleListPage(page)
+    list_page.expect_to_be_loaded()
+    list_page.expect_to_have_articles([markdown_article])
 
-    # He can find the marked up content in the article
-    marked_up = article_element.locator("strong")
+    # He sees that the articles have markdown
+    article_content = list_page.get_content_for_article(markdown_article)
+    marked_up = article_content.locator("strong")
     expect(marked_up).to_have_text("markdown")
 
     # He can also find the marked up content in the article page
-    article_link = page.get_by_role("link", name=markdown_article.title)
-    article_link.click()
+    article_page = list_page.visit_article(markdown_article)
+    article_page.expect_to_be_for_article(markdown_article)
 
-    # The article page has the title of the article
-    expect(page).to_have_title(re.compile(markdown_article.title))
-    marked_up = page.get_by_role("article").locator("strong")
+    article_content = article_page.get_article_content()
+    marked_up = article_content.locator("strong")
     expect(marked_up).to_have_text("markdown")
 
 
@@ -199,6 +98,10 @@ def test_markdown_navpage(page, server_url, about_page):
     # L goes to visit the about page
     page.goto(server_url + about_page.get_absolute_url())
 
+    about_page_page = NavPagePage(page)
+    about_page_page.expect_to_be_for_nav_page(about_page)
+
     # He can find the marked up content in the article
-    marked_up = page.locator("strong")
+    page_content = about_page_page.get_page_content()
+    marked_up = page_content.locator("strong")
     expect(marked_up).to_have_text("markdown")
