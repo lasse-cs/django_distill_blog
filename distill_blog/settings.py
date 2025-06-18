c1ad4b504ec97cb06d06b2b9f756e871796fd8e5
lasse-cs <lasse.schmieding@gmail.com>
2025-06-18 18:14:17 +0100

    Try static files again

--- distill_blog/settings.py
+++ distill_blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# TODO: Should manage this with an environment variable I think
-STATIC_URL = "django_distill_blog/static/"
-STATIC_ROOT = BASE_DIR / "static_output"
+STATIC_URL = os.getenv("SITE_ROOT", "") + "static/"
+if os.getenv("STATIC_ROOT"):
+    STATIC_ROOT = Path(os.getenv("STATIC_ROOT"))
+else:
+    STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
